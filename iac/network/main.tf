bf30daaf73c31166f46ffce25bbe642eadf8eef6
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-30 21:19:52 +1000

    Add files via upload

--- iac/network/main.tf
+++ iac/network/main.tf
@@ -258,7 +258,7 @@ module "public-route53" {
   createrecord = true
   
 
-  /*zones = {
+  zones = {
       "public-vpc" = {
             domain_name = var.aws_route53_public_zone_name
             vpc = [
@@ -267,7 +267,7 @@ module "public-route53" {
               }
             ]
       }
-  }*/
+  }
 
   zone_name = var.aws_route53_public_zone_id
   #zone_id = var.aws_route53_public_zone_id
