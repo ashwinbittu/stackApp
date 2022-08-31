56394e05ec624bab5fbcb3181e03839223d63e50
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-31 23:59:47 +1000

    Delete message directory

--- message/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "message-launch-temmplate_id" {
-  value = module.message-launch-template.launch_template_id
-}
-
-output "message-asg_id" {
-  value = module.message-asg.autoscaling_group_id
-}
